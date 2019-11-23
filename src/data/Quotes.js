868a4f0bb315bf5b930151d89c43724a19696109
Sohel Shaikh <rebht78@gmail.com>
2019-11-23 20:55:09 +0530

    added five quotes from Sherlock Novel

---
+++ src/data/Quotes.js
@@ -0,0 +1,25 @@
+const Quotes = [
+    {
+        text:'There is nothing more deceptive than an obvious fact.',
+        author:'The Boscombe Valley Mystery'
+    },
+    {
+        text:'You have a grand gift for silence, Watson. It makes you quite invaluable as a companion.',
+        author:'The Complete Sherlock Holmes'
+    },
+    {
+        text:'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
+        author:'His Last Bow'
+    },
+    {
+        text:'To a great mind, nothing is little',
+        author:'The Study in Scarlet'
+    },
+    {
+        text:'I wanted to end the world, but I\'ll settle for ending yours.',
+        author:'Professor Moriarty'
+    },
+    
+];
+
+export default Quotes;
