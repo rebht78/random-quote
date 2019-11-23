3602a07af4cf759054add34cae01f17f46d35f5c
Sohel Shaikh <rebht78@gmail.com>
2019-11-23 21:51:11 +0530

    added footer and minor changes to index.html

--- src/components/App.components.jsx
+++ src/components/App.components.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import Quotes from '../data/Quotes';
+import './App.styles.css';
 import Quote from './Quote.components';
 
-class App extends React.Component
-{
-    constructor(props)
-    {
+class App extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
             quote: Quotes[0]
@@ -13,12 +12,17 @@ class App extends React.Component
     }
     render() {
         return (
-            <Quote text={this.state.quote.text} author={this.state.quote.author} generateQuote={this.newQuote}/>
+            <div>
+                <Quote text={this.state.quote.text} author={this.state.quote.author} generateQuote={this.newQuote} />
+                <footer id="text-footer">
+                Crafted in Surat with <i class="fa fa-heart"></i> by Sohel Shaikh
+                </footer>
+            </div>
         )
     }
     newQuote = (e) => {
         const randomIndex = Math.floor(Math.random() * Quotes.length);
-        this.setState({quote:Quotes[randomIndex]});
+        this.setState({ quote: Quotes[randomIndex] });
     }
 }
 
